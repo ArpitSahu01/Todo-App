e4189ef4f8d9453096f7108eda3bbb01d889afc1
arpit <arpitsahu789@gmail.com>
2023-03-12 18:31:55 +0530

    done with creating add cart button

--- lib/app/modules/home/view.dart
+++ lib/app/modules/home/view.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:to_do_list/app/modules/home/controller.dart';
+import 'package:to_do_list/app/modules/home/widgets/add_cart.dart';
 
 class HomePage extends GetView<HomeController> {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Home Screen"),),
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(12),
+              child: Text(
+                'My List',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            
+            GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+              children: [AddCard()],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
